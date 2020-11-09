c18a0e759a8fa14a3b80cd2b07f7b5c327d0d189
Sungjun <opr99@naver.com>
2020-11-09 21:34:52 +0900

    Create 23.국영수.py

---
+++ ch14/23.국영수.py
@@ -0,0 +1,12 @@
+n = int(input())
+array = []
+
+for i in range(n):
+    data = input().split()
+    array.append((data[0], int(data[1]), int(data[2]), int(data[3])))
+
+
+array.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for item in array:
+    print(item[0])
