33baeff9d67de68f163cc974445d731b9527a163
Sungjun <opr99@naver.com>
2020-11-23 23:53:13 +0900

    Create 42.탑승구.py

--- ch18/42. 탑승구.py
+++ ch18/42. 탑승구.py
@@ -15,32 +15,23 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 # 노드의 개수와 간선(Union 연산)의 개수 입력 받기
-v, e = map(int, input().split())
-parent = [0] * (v + 1) # 부모 테이블 초기화하기
+v = int(input())
+e = int(input())
 
-# 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
-edges = []
-result = 0
+parent = [0] * (v + 1)  # 부모 테이블 초기화하기
 
 # 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, v + 1):
     parent[i] = i
 
-# 모든 간선에 대한 정보를 입력 받기
+# Union 연산을 각각 수행
+result = 0
 for _ in range(e):
-    a, b, cost = map(int, input().split())
-    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
-    edges.append((cost, a, b))
-
-# 간선을 비용순으로 정렬
-edges.sort()
-
-# 간선을 하나씩 확인하며
-for edge in edges:
-    cost, a, b = edge
-    # 사이클이 발생하지 않는 경우에만 집합에 포함
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result += cost
+    data = find_parent(parent, int(input()))
+    if data == 0:
+        break
+    else:
+        union_parent(parent, data, data - 1)
+        result += 1
 
 print(result)
