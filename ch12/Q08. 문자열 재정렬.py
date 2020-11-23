d8fd513028c948a9d625f1c00699a803ef9203ba
Sungjun <opr99@naver.com>
2020-11-24 03:31:12 +0900

    Create Q08.py

--- ch12/Q08. 문자열 재정렬.py
+++ ch12/Q08. 문자열 재정렬.py
@@ -3,15 +3,14 @@ letters = []
 digits = 0
 
 for data in array:
-    if data.isdigit():  # isalpha()도 존재함!
+    if data.isdigit():  # isalpha(), isalnum()도 존재함!
         digits += int(data)
     else:
         letters.append(data)
 
 letters.sort()
 
-# 예외처리를 생각 못함!
-if digits != 0:
+if digits != 0:  # 예외처리를 생각 못함!
     letters.append(str(digits))
 
 print(''.join(letters))
