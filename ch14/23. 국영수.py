d4443034c6e4bfe2632ab0057689afb57cd8ddc2
Sungjun <opr99@naver.com>
2020-11-24 13:51:08 +0900

    Create Q23.py

--- ch14/23. 국영수.py
+++ ch14/23. 국영수.py
@@ -5,8 +5,13 @@ for i in range(n):
     data = input().split()
     array.append((data[0], int(data[1]), int(data[2]), int(data[3])))
 
-
 array.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
 
+'''
+for _ in range(n):
+    array.append(input().split())
+
+array.sort(key = lambda x: (-int(x[1], int(x[2]), -int(x[3]), x[0])
+'''
 for item in array:
     print(item[0])
