9dda0596a5e8739bea3d77ed6ce3b3cd5a9a0b7a
Sungjun <opr99@naver.com>
2020-11-27 21:37:51 +0900

    Create Q15.py

--- ch16/Q31. 금광.py
+++ ch16/Q31. 금광.py
@@ -1,12 +1,12 @@
-# 테스트 케이스(Test Case) 입력
-for tc in range(int(input())):
-    # 금광 정보 입력
+for t in range(int(input())):
     n, m = map(int, input().split())
-    array = list(map, input().split())
-    dp = []
+    array = list(map(int, input().split()))
+
+    # dp 테이블 만들기
+    dp = [[] for _ in range(n)]
     index = 0
     for i in range(n):
-        dp.append(array[index:index+m])
+        dp[i] = array[index:index + m]
         index += m
 
     # 다이나믹 프로그래밍 진행
@@ -17,17 +17,21 @@ for tc in range(int(input())):
                 left_up = 0
             else:
                 left_up = dp[i - 1][j - 1]
-            # 왼쪽 아래에서 오는 경우
+
+            # 왼쪽 아래서 오는 경우
             if i == n - 1:
                 left_down = 0
             else:
                 left_down = dp[i + 1][j - 1]
+
             # 왼쪽에서 오는 경우
             left = dp[i][j - 1]
-            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+            # 점화식
+            dp[i][j] = dp[i][j] + max(left, left_up, left_down)
 
+    # 마지막 열의 가장 큰 값 찾기
     result = 0
     for i in range(n):
         result = max(result, dp[i][m - 1])
 
-    print(dp)
+    print(result)
