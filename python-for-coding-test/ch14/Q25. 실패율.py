ad7e84fb709b3a95a047e3c14784aac72f27ea71
Sungjun <opr99@naver.com>
2020-12-29 17:14:56 +0900

    Create Q25. 실패율.py

--- python-for-coding-test/ch14/Q25. 실패율.py
+++ python-for-coding-test/ch14/Q25. 실패율.py
@@ -1,7 +1,8 @@
 def solution(N, stages):
     answer = []
-    child = [0] * N
-    parent = [0] * N
+    child = [0] * N  # 실패율의 분자
+    parent = [0] * N  # 실패율의 분모
+
     for i in range(len(stages)):
         for j in range(N):
             if stages[i] >= j + 1:
@@ -9,14 +10,16 @@ def solution(N, stages):
             if stages[i] == j + 1:
                 child[j] += 1
 
+    # 실패율 리스트
     rate = []
     for i in range(N):
         result = (child[i] / parent[i]) if parent[i] != 0 else 0
-        rate.append((result, i + 1))
-    print(rate)
+        rate.append((result, i + 1))  # 리스트에 (실패율, 스테이지 번호) 삽입
 
-    rate.sort(key=lambda x: (-x[0], x[1]))
+    # 실패율을 기준으로 내림차순 정렬
+    rate.sort(key=lambda x: -x[0])
 
+    # 정렬된 스테이지 번호를 answer 리스트에 삽입
     for item in rate:
         answer.append(item[1])
 
