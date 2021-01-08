7f8d7a5e7e23dcbc64846f1c874f5ed286cf15d1
Sungjun <opr99@naver.com>
2021-01-08 20:31:39 +0900

    Create #2579

--- baekjoon/2579(계단 오르기).py
+++ baekjoon/2579(계단 오르기).py
@@ -1,16 +1,13 @@
 n = int(input())
 
-data = [0]
-for i in range(n):
-    data.append(int(input()))
+data = [0] * 301
+for i in range(1, n + 1):
+    data[i] = int(input())
 
-dp = [0] * (n + 1)
+dp = [0] * 301
 dp[1] = data[1]
 dp[2] = data[1] + data[2]
-dp[3] = data[1] + data[3]
-
-for i in range(4, n + 1):
+for i in range(3, n + 1):
     dp[i] = max(dp[i - 2] + data[i], dp[i - 3] + data[i - 1] + data[i])
 
-for i in range(1, n + 1):
-    print(dp[i])
+print(dp[n])
