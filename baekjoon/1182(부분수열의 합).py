de5985ba20c5a9a88073ba259b2899ab723c2980
sungjun1116 <opr99@naver.com>
2021-01-08 13:33:36 +0900

    Create #1182

--- baekjoon/1182(부분수열의 합).py
+++ baekjoon/1182(부분수열의 합).py
@@ -1,1 +1,12 @@
-ㄴㅇㄴㅇㄴ
+import itertools
+
+n, s = map(int, input().split())
+data = list(map(int, input().split()))
+
+count = 0
+for i in range(1, n + 1):
+    for x in itertools.combinations(data, i):
+        if sum(list(x)) == s:
+            count += 1
+
+print(count)
