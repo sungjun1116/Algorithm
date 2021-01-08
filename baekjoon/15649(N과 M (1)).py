0a6041dbcbb11c4dd48532b00f73996ae17a54ad
sungjun1116 <opr99@naver.com>
2021-01-08 13:51:58 +0900

    Create #15649

--- baekjoon/15649(N과 M (1)).py
+++ baekjoon/15649(N과 M (1)).py
@@ -3,12 +3,9 @@ from itertools import *
 n, m = map(int, input().split())
 data = [i for i in range(1, n + 1)]
 
-result = list(permutations(data, m))
-# n, m = 4, 2 => [(1, 2), (1, 3), (1, 4), (2, 1), (2, 3), (2, 4), (3, 1), (3, 2), (3, 4), (4, 1), (4, 2), (4, 3)]
-
-for i in range(len(result)):
-    for j in range(len(result[i])):
-        # result[i][j] = "("  1  ","  2  ")"
-        if str(result[i][j]).isnumeric():
-            print(result[i][j], end=' ')
+for x in permutations(data, m):
+    result = list(x)
+    for i in result:
+        print(i, end=' ')
     print()
+
