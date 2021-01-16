346d3e9726da73450e34e1616c4a6273d656e641
sungjun1116 <opr99@naver.com>
2021-01-16 22:17:38 +0900

    Create #2156

--- baekjoon/2156(포도주 시식).py
+++ baekjoon/2156(포도주 시식).py
@@ -1,8 +1,9 @@
 n = int(input())
 
-data = [0]
-for _ in range(n):
-    data.append(int(input()))
+data = [0] * 10001
+for i in range(1, n + 1):
+    data[i] = int(input())
+
 
 dp = [0] * 10001
 dp[1] = data[1]
@@ -10,6 +11,6 @@ dp[2] = data[1] + data[2]
 
 for i in range(3, n + 1):
     dp[i] = max(dp[i - 2] + data[i], dp[i - 3] + data[i - 1] + data[i])
-    dp[i] = max(dp[i - 1], dp[i])  # 마지막 포도주를 무조건 안마셔도 되기 때문에 추가
+    dp[i] = max(dp[i - 1], dp[i])  # 포도주를 2번연속 안 먹을 경우가 존재하기 때문에 추가
 
 print(dp[n])
