b4cf628abcb45a3d753e40026129aa69d35e9bb5
sungjun1116 <opr99@naver.com>
2021-01-18 02:10:15 +0900

    Create #16236

--- baekjoon/16236(아기 상어).py
+++ baekjoon/16236(아기 상어).py
@@ -1,40 +1,40 @@
 from collections import deque
-INF = 1e9
+
+INF = int(1e9)
 
 n = int(input())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
 
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
 
 now_size = 2
-now_x = 0
-now_y = 0
+now_x, now_y = 0, 0
 
+# 아기 상어의 시작 위치를 찾은 뒤에 그 위치엔 아무것도 없다고 처리
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 9:
+        if array[i][j] == 9:
             now_x, now_y = i, j
-            graph[now_x][now_y] = 0
+            array[now_x][now_y] = 0
 
-# 이동할 네 방향 정의(상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 
-# 최단거리 테이블을 만들기 위핸 bfs 함수
+# 모든 위치까지의 '최단 거리만' 계산하는 BFS 함수
 def bfs():
     dist = [[-1] * n for _ in range(n)]
-    q = deque([])
+    q = deque([(now_x, now_y)])
+    dist[now_x][now_y] = 0
     while q:
-        x, y = q.popleft([(now_x, now_y)])
+        x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n:
-                # 자신의 크기보다 작거나 같은 경우에 지나갈 수 있음
-                if dist[nx][ny] == -1 and graph[nx][ny] <= now_size:
-                    dist[nx][ny] = dist[nx][ny] + 1
+                if dist[nx][ny] == -1 and array[nx][ny] <= now_size:
+                    dist[nx][ny] = dist[x][y] + 1
                     q.append((nx, ny))
     return dist
 
@@ -46,17 +46,20 @@ def find(dist):
     for i in range(n):
         for j in range(n):
             # 도달이 가능하면서 먹을 수 있는 물고기일 때
-            if dist[i][j] != -1 and dist[i][j] < min_dist and 1 <= graph[i][j] < now_size:
-                x, y = i, j
-                min_dist = dist[i][j]
+            if dist[i][j] != -1 and 1 <= array[i][j] < now_size:
+                # 가장 가까운 물고기 1마리만 선택
+                if dist[i][j] < min_dist:
+                    x, y = i, j
+                    min_dist = dist[i][j]
     if min_dist == INF:
         return None
     else:
-        return x, y, min_dist
+        return x, y, min_dist  # 먹을 물고기의 위치와 최단 거리
 
 
 result = 0
-ate = 0  # 현재 크기에서 먹은 양
+ate = 0
+
 while True:
     # 먹을 수 있는 물고기의 위치 찾기
     value = find(bfs())
@@ -64,12 +67,11 @@ while True:
         print(result)
         break
     else:
-        # 현재 위치 갱신 및 이동 거리 변경
         now_x, now_y = value[0], value[1]
         result += value[2]
-        # 먹은 위치에는 이제 아무것도 없도록 처리
-        graph[now_x][now_y] = 0
+        array[now_x][now_y] = 0
         ate += 1
+        # 자신의 현재 크기 이상으로 먹은 경우, 크기 증가
         if ate >= now_size:
             now_size += 1
             ate = 0
