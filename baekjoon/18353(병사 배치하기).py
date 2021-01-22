d8a44d833069ef162f5ff7cd0a88fffc984952d2
sungjun1116 <opr99@naver.com>
2021-01-22 20:22:40 +0900

    Create #18353

---
+++ baekjoon/18353(병사 배치하기).py
@@ -0,0 +1,14 @@
+# LIS(가장 긴 증가하는 부분 수열)
+n = int(input())
+array = list(map(int, input().split()))
+
+array.reverse()
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+print(n - max(dp))
