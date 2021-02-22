b64b3fa3ab1fb6c4334b1c62d11033318003b4ce
sungjun1116 <opr99@naver.com>
2021-02-22 21:37:35 +0900

    Update

--- python-for-coding-test/ch3/4. 1이 될때까지.py
+++ python-for-coding-test/ch3/4. 1이 될때까지.py
@@ -1,16 +1,17 @@
 n, k = map(int, input().split())
 result = 0
 '''
-while n >= k:
-    while n % k != 0:
+while True:
+    if n % k == 0:
+       n = n // k
+    else:
         n -= 1
-        result += 1
-    n //= k
     result += 1
-# 마지막으로 남은 수에 대하여 1씩 빼기
-while n > 1:
-    n -= 1
-    result += 1
+
+    if n == 1:
+        break
+
+print(result)
 '''
 # 빠른 풀이
 while True:
