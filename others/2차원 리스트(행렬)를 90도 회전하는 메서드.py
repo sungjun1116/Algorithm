d42c75a65cd862e6fcd97a839d045673dd8aaec5
sungjun1116 <opr99@naver.com>
2021-02-22 15:43:55 +0900

    Create #roate_a_matrix_90_degree

--- others/2차원 리스트(행렬)를 90도 회전하는 메서드.py
+++ others/2차원 리스트(행렬)를 90도 회전하는 메서드.py
@@ -1,12 +1,6 @@
-def rotate_a_matrix_by_90_degree(a):
-    row_length = len(a)
-    column_length = len(a[0])
-
-    res = [[0] * row_length for _ in range(column_length)]
-    for r in range(row_length):
-        for c in range(column_length):
-            res[c][row_length - 1 - r] = a[r][c]
-    return res
+def rotate_a_matrix_by_90_degree(paper):
+    r, c = len(paper), len(paper[0])
+    return [[paper[r - 1 - j][i] for j in range(r)] for i in range(c)]
 
 
 a = [
