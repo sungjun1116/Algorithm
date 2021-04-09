8a4515f50ed80abf115b4a4895ab20a7a11b56ba
sungjun1116 <opr99@naver.com>
2021-04-09 15:06:25 +0900

    Create #1654

---
+++ baekjoon/1654(랜선 자르기).py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+data = [int(input()) for _ in range(k)]
+
+start, end = 1, max(data)
+result = 0
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+    for i in data:
+        total += i // mid
+
+    if total >= n:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)
